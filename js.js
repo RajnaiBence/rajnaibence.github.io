2d750f771ac9c621a3a885f62f3bf856abc3020d
Rajnai Bence <rajnai.bence99@gmail.com>
2023-12-09 20:36:55 +0100

    server side images

--- js.js
+++ js.js
@@ -2,11 +2,17 @@ function $(id) {
     return document.getElementById(id);
 }
 
-
 function kep(){
-    $('main').innerHTML = '<img src="image/3x.gif" alt="kep">';
+    var fs = require('fs');
+    var files = fs.readdirSync('/image/');
 
-
-    
-   
+    for (let index = 0; index < files.length; index++) {
+        $('main').innerHTML += '<img class="kepek" src='+folder[index]+' alt="kep">';    
+        
+    }
 }
+
+
+window.onload = function() {
+    kep();
+};
