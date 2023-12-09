426565df079df6f89190539c19aa8c88a1db9e87
Rajnai Bence <rajnai.bence99@gmail.com>
2023-12-09 22:10:14 +0100

    download demo

--- js.js
+++ js.js
@@ -2,17 +2,3 @@ function $(id) {
     return document.getElementById(id);
 }
 
-function kep(){
-
-    var files = fs.readdirSync('/image/');
-
-    for (let index = 0; index < files.length; index++) {
-        $('main').innerHTML += '<img class="kepek" src='+folder[index]+' alt="kep">';    
-        
-    }
-}
-
-
-window.onload = function() {
-    kep();
-};
